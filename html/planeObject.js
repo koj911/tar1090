d713ab2aa66d460a6b9c9eadf8f524f55c495b71
koj911 <59871282+koj911@users.noreply.github.com>
2024-11-30 16:29:31 +0900

    Added support for abbreviating RC-12, TC-12, and MC-12

--- html/planeObject.js
+++ html/planeObject.js
@@ -850,7 +850,7 @@ PlaneObject.prototype.shortenAircraftType = function() {
         return match[1];
     } else if (/^AEROSPATIALE\s/i.test(type)) {
         return type.replace(/AEROSPATIALE\s([\w-]+).*/i, "$1");
-    } else if (match = type.match(/^(Beech(?:craft)?)\s(U?C-12\S*).*/i)) {
+    } else if (match = type.match(/^(Beech(?:craft)?)\s(T?M?R?U?C-12\S*).*/i)) {
         return match[2];
     } else if (match = type.match(/^CESSNA\s(UC-35\S*).*/i)) {
         return match[1];
